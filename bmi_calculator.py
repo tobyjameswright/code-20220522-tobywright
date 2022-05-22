8b5794527a2020af1b5f6b852ea5e9956abeffa0
tobyjameswright <tobyjameswright@hotmail.co.uk>
2022-05-22 12:51:26 +0100

    add function to calculate bmi as BMI calculation requires the inclusion of height in M but the original data has Cm this has been converted in a seperate helper function

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -18,9 +18,32 @@ def load_csv_data(filename: str) -> pd.DataFrame:
     return data
 
 
+def calculate_height_m(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Function to convert Height from CM to M
+    """
+    data["HeightM"] = data["HeightCm"] / 100
+    return data
+
+
+def calculate_bmi(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Function to calculate BMI via
+    BMI = WeightKg / (HeightM) ^ 2
+    1. HeightM is calculated
+    2. BMI is calculated as above formula
+    3. HeightM column is dropped
+    """
+    data = calculate_height_m(data)
+    data["BMI"] = data["WeightKg"] / (data["HeightM"] ** 2)
+    data.drop("HeightM", axis=1, inplace=True)
+    return data
+
+
 def main() -> None:
     patient_df = load_json_data("patient_data")
     bmi_cat_risk = load_csv_data("bmi_categories")
+    patient_df_bmi = calculate_bmi(patient_df)
 
 
 if __name__ == "__main__":
