2157f0a244bbc74a413f4aee865a04342aa4434c
tobyjameswright <tobyjameswright@hotmail.co.uk>
2022-05-22 13:31:38 +0100

    add test for calculating bmi category

--- tests/test_bmi_calcs/test_bmi_calc.py
+++ tests/test_bmi_calcs/test_bmi_calc.py
@@ -1,5 +1,5 @@
 # Toby Wright 2022
-from bmi_calculator import calculate_height_m, calculate_bmi
+from bmi_calculator import calculate_height_m, calculate_bmi, calculate_bmi_category
 import pandas as pd
 
 
@@ -17,3 +17,12 @@ def test_calculate_bmi():
     )
     updated_df = calculate_bmi(test_df)
     assert updated_df["BMI"].iloc[0] == 82
+
+
+def test_calculate_bmi_category():
+    test_df = pd.DataFrame.from_dict(
+        {"Gender": ["Male"], "HeightCm": [100], "WeightKg": [82]}
+    )
+    df_bmi = calculate_bmi(test_df)
+    updated_df = calculate_bmi_category(df_bmi)
+    assert updated_df["BMI_category"].iloc[0] == "Very severely obese"
