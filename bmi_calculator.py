d449eb89ea70adf2e7146df125e066c8cbd9552a
tobyjameswright <tobyjameswright@hotmail.co.uk>
2022-05-22 12:49:05 +0100

    adding function to read csv data for bmi categories

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -10,8 +10,17 @@ def load_json_data(filename: str) -> pd.DataFrame:
     return data
 
 
+def load_csv_data(filename: str) -> pd.DataFrame:
+    """
+    Function to load a CSV file into a pandas dataframe
+    """
+    data = pd.read_csv(f"{filename}.csv")
+    return data
+
+
 def main() -> None:
     patient_df = load_json_data("patient_data")
+    bmi_cat_risk = load_csv_data("bmi_categories")
 
 
 if __name__ == "__main__":
